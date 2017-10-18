213172e247f9870900c3a87bb40264e3440f6ed7
Loaye <jr.carstens00@gmail.com>
2017-10-18 16:49:32 -0700

    adds two additional tests to test file to test fib

--- test_series.py
+++ test_series.py
@@ -14,3 +14,13 @@ def test_fib_two():
     '''Test fib function to check value 2 of fibonacci'''
     from series import fib
     assert fib(2) == 1
+
+def test_fib_five():
+    '''Test fib function to check value 5 of fibonacci'''
+    from series import fib
+    assert fib(5) == 3
+
+def test_fib_seven():
+    '''Test fib function to check value 7 of fibonacci'''
+    from series import fib
+    assert fib(7) == 8
