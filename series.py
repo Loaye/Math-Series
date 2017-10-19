e4f6241fcb1c3e8b373d00c7631d15d5df8bf81a
Loaye <jr.carstens00@gmail.com>
2017-10-18 18:01:35 -0700

    adds final sum_series function to find any value with any given sequence

--- series.py
+++ series.py
@@ -23,5 +23,16 @@ def lucas(n):
     else:
         return fib(n - 1) + fib(n + 1)
 
+
 def sum_series(n, x=0, y=1):
-    pass
+    '''sum_series function to find any value in any sequence given'''
+    if n == 0:
+        return 0
+    else:
+        series_list = [x, y]
+        for i in range(n):
+            z = (x + y)
+            x = y
+            y = z
+            series_list.append(z)
+        return series_list[n - 1]
